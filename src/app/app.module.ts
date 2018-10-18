a944264677782aa9bed008d2559889cc3e02ef21
Johan <jach_1997@hotmail.com>
2018-10-18 00:53:56 -0500

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './Introduction/home/home.component';
+import {ContactoComponent} from './Introduction/contacto/contacto.component';
+import {CochesComponent} from './coches/coches.component';
+import {HomecomponentComponent} from './homecomponent/homecomponent.component';
 
+const rutas: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'coches', component: CochesComponent},
+  {path: 'coches/:marca/:modelo', component: CochesComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent,
+    CochesComponent,
+    HomecomponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/coches/coches.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+
+@Component({
+  selector: 'app-coches',
+  templateUrl: './coches.component.html',
+  styleUrls: ['./coches.component.css']
+})
+export class CochesComponent implements OnInit {
+  coche: { marca: string, modelo: string };
+
+  constructor(private rutaActiva: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.coche = {
+      marca: this.rutaActiva.snapshot.params.marca,
+      modelo: this.rutaActiva.snapshot.params.modelo
+    };
+    this.rutaActiva.params.subscribe(
+      (params: Params) => {
+        this.coche.modelo = params.modelo;
+        this.coche.marca = params.marca;
+      }
+    );
+  }
+
+}
